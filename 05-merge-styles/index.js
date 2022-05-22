9884839e6e11ecb41eb6856d6a883608243dc311
Elem <elem15@yandex.ru>
2022-05-22 18:04:20 +0300

    feat: solve 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+const arr = [];
+
+fs.readdir(path.join(__dirname, 'styles'), async (err, files) => {
+  if (err) console.log(err);
+  for (const file of files) {
+    const src = path.join(__dirname, 'styles', file);
+    const extname = path.extname(src);
+    let data = '';
+    if (extname === '.css') {
+      const stream = fs.createReadStream(src, 'utf-8');
+      stream.on('data', chunk => {
+        data += chunk;
+      });
+      stream.on('end', () => arr.push(data));
+      stream.on('error', (err) => console.log(err));
+    }
+  }
+});
+
+process.on('exit', () => {
+  output.write(arr.join('\n'));
+  output.on('error', (err) => console.log(err));
+});
